90dc96057ad267dd0f2256cc82500e27dfedf284
derfelot <frederic.koch@gmail.com>
2019-03-31 16:18:29 +0200

    lilac: Omnify

--- omni_lilac.mk
+++ omni_lilac.mk
@@ -6,17 +6,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/sony/lilac/device.mk)
 
 ### BOOTANIMATION
-# vendor/lineage/config/common_full_phone.mk
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
-# vendor/lineage/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
-### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+### OMNI
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_lilac
+PRODUCT_NAME := omni_lilac
 PRODUCT_DEVICE := lilac
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := G8441
@@ -28,10 +27,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Sony/lilac/lilac:9/YOSHINO-2.2.0-190326-1903/1:user/dev-keys
 
-ifneq ($(LINEAGE_DEV_CERTIFICATE),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
-endif
-
-ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
-    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
-endif
